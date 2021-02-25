cfc1dba6fab4b01f943f8ad50b5e23e48a1396f3
b6320502347 <78533381+b6320502347@users.noreply.github.com>
2021-02-25 16:40:39 +0700

    1

--- 6320502347_4_1.c
+++ 6320502347_4_1.c
@@ -15,18 +15,21 @@ int main(){
             days[0]=day%100;
             days[1]=exp%100;
             }
-    if(month[1]>12||month[0]>12||days[1]>31||days[0]>31||((days[1]>30||days[0]>30)&&((month[1]==4||month[1]==6||month[1]==8||month[1]==11)||(month[0]==4||month[0]==6||month[0]==8||month[0]==11)))||((days[1]>29||days[0]>29)&&(month[0]==2||month[1]==2)))
-        printf("W");
-    else if(year[1]>year[0])
-        printf("B");
-    else if(year[1]==year[0])
-                if(month[1]>month[0])
-                    printf("B");
-                else if(month[1]==month[0])
-                            if(days[1]>=days[0])
-                                    printf("B");
-                            else  printf("L");
-                else  printf("L");
-    else   if(year[1]<year[0])
-        printf("L");
+    if((month[1]<=12&&month[1]>0)||(month[0]<=12&&month[0]>0)||(days[1]<=31&&days[1]>0)||(days[0]<=31&&days[0]>0)){
+        if(((days[1]>30||days[0]>30)&&((month[1]==4||month[1]==6||month[1]==8||month[1]==11)||(month[0]==4||month[0]==6||month[0]==8||month[0]==11)))||((days[1]>29||days[0]>29)&&(month[0]==2||month[1]==2)))
+            printf("W");
+        else if(year[1]>year[0])
+            printf("B");
+        else if(year[1]==year[0])
+                    if(month[1]>month[0])
+                        printf("B");
+                    else if(month[1]==month[0])
+                                if(days[1]>=days[0])
+                                        printf("B");
+                                else  printf("L");
+                    else  printf("L");
+        else   if(year[1]<year[0])
+            printf("L");
+        }
+    else printf("W");
     }
