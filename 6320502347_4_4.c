6e34ccef08ec3151dc4ad7b8288b4c2b08d49b90
b6320502347 <78533381+b6320502347@users.noreply.github.com>
2021-03-04 17:43:19 +0700

    ..

--- 6320502347_4_4.c
+++ 6320502347_4_4.c
@@ -1,16 +1,17 @@
 #include <string.h>
 #include <stdio.h>
-
 int main () {
+    int i=0;
    char str[1000] ;
-   gets(str);
+   scanf("%[^\n]s",str);
    char *token;
    token = strtok(str," ");
    while( token != NULL ) {
-        if(strcmp(token,"i")!=0&&strcmp(token,"of")!=0&&strcmp(token,"the")!=0&&strcmp(token,"on")!=0&&strcmp(token,"at")!=0&&strcmp(token,"for")!=0&&strcmp(token,"with")!=0&&strcmp(token,"a")!=0&&strcmp(token,"an")!=0&&strcmp(token,"and")!=0&&strcmp(token,"in")!=0)
-        printf("%c\n",token[0]-32);
+        if(i==0)
+        printf("%c",token[0]-32);
+        else if(strcmp(token,"i")!=0&&strcmp(token,"of")!=0&&strcmp(token,"the")!=0&&strcmp(token,"on")!=0&&strcmp(token,"at")!=0&&strcmp(token,"for")!=0&&strcmp(token,"with")!=0&&strcmp(token,"a")!=0&&strcmp(token,"an")!=0&&strcmp(token,"and")!=0&&strcmp(token,"in")!=0)
+        printf("%c",token[0]-32);
+        i++;
       token = strtok(NULL," ");
    }
-
-   return(0);
 }
