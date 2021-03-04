6551eb7b7577666bcfd2bdb6ca052e16b78ed9c0
b6320502347 <78533381+b6320502347@users.noreply.github.com>
2021-03-04 16:09:26 +0700

    -

--- 6320502347_4_4.c
+++ 6320502347_4_4.c
@@ -7,7 +7,8 @@ int main () {
    char *token;
    token = strtok(str," ");
    while( token != NULL ) {
-        printf("%c",token[0]-32);
+        if(strcmp(token,"i")!=0&&strcmp(token,"of")!=0&&strcmp(token,"the")!=0&&strcmp(token,"on")!=0&&strcmp(token,"at")!=0&&strcmp(token,"for")!=0&&strcmp(token,"with")!=0&&strcmp(token,"a")!=0&&strcmp(token,"an")!=0&&strcmp(token,"and")!=0&&strcmp(token,"in")!=0)
+        printf("%c\n",token[0]-32);
       token = strtok(NULL," ");
    }
 
