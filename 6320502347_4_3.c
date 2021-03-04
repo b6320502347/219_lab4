ebd4a5565130ef7b643b0d9a9b4d7aaa5524df40
b6320502347 <78533381+b6320502347@users.noreply.github.com>
2021-03-04 17:40:00 +0700

    --

--- 6320502347_4_3.c
+++ 6320502347_4_3.c
@@ -2,14 +2,13 @@
 int main(){
 char key[200],i;
 scanf("%s",key);
-for(i=0;i<200;i++){
-    if(i==0){
-    printf("%c",key[i]);
+for(i=0;i<200;i++)
+    if(key[i]!='\0'){
+        if(i==0)
+            printf("%c",key[i]);
+        else if(key[i]!=key[i-1])
+            printf("%c",key[i]);
+            }
+    else break;
     }
-    else if(key[i]!=key[i-1]){
-        if(key[i]=='\0')
-        break;
-        printf("%c",key[i]);
-        }
-    }
-}
+
